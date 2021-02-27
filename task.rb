44c69ad15dc830fccb8a5589c1a62ccbbc75b2f4
y.watanabe <trance3912hkss@gmail.com>
2021-02-27 11:37:42 +0900

    q8~q11を提出

--- task.rb
+++ task.rb
@@ -69,7 +69,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -79,21 +80,34 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if( food.include?("うに") ) then
+      puts "#{food}:好物です"
+    else
+      puts "#{food}:まあまあ好きです"
+    end
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  newsports = sports.flatten.uniq
+  puts "ユーザーの趣味一覧"
+  
+  newsports.each.with_index(1) do |sport, i|
+    puts "No.#{i} #{sport}"
+  end
 end
 
 def q12
